af37dab7f64c2cee5be85dcc0f51422587c0d9be
nokcharathae <rkwmal0000@naver.com>
2022-04-06 09:16:07 +0900

    upload

--- HW1_Transformation/2019204035.js
+++ HW1_Transformation/2019204035.js
@@ -1,6 +1,5 @@
 import * as THREE from '/node_modules/three/build/three.module.js';
 
-
 const camera=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
 const scene=new THREE.Scene();
 
@@ -9,40 +8,22 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setViewport(0,0,window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.set(0,0,100);
+camera.position.set(0,0,30);
 camera.lookAt(0,0,0);
 camera.up.set(0,1,0);
 
-const goe_box = new THREE.BoxGeometry(5,5,5);
+const geo_box = new THREE.BoxGeometry(5,5,5);
 const loader = new THREE.TextureLoader();
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshPhongMaterial({
   map: loader.load('black.png'),
 });
-const boxobj=new THREE.Mesh(goe_box,material);
+const boxobj=new THREE.Mesh(geo_box,material);
 
-const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 50, 50);
 
 scene.add(boxobj);
 scene.add(light);
-
-/* HW1. Transformation
-
-1. 월드에 큐브를 만들어 배치하고 lighting 과 texture mapping 적용
-2. 키보드 버튼 press 이벤트 추가
-
-(60점)
- - 'r' :  큐브의 x 축 방향으로 3도 rotation
- - 't' :  큐브의 y 축 방향으로 3도 rotation
- - 'y' :  큐브의 z 축 방향으로 3도 rotation
- - 'f' :  큐브의 x 축 방향으로 -3도 rotation
- - 'g' :  큐브의 y 축 방향으로 -3도 rotation
- - 'h' :  큐브의 z 축 방향으로 -3도 rotation
-(40점)
- - 'a' :  큐브를 화면의 왼쪽 방향으로 10 pix 만큼 평행 이동
- - 'd' :  큐브를 화면의 오른쪽 방향으로 10 pix 만큼 평행 이동
- - 'w' :  큐브를 화면의 위쪽 방향으로 10 pix 만큼 평행 이동
- - 's' :  큐브를 화면의 아래쪽 방향으로 10 pix 만큼 평행 이동
-*/
 
 
 let screensizeW=window.innerWidth;
@@ -76,12 +57,11 @@ window.addEventListener("keypress", checkKeyPressed, false);
 
 function checkKeyPressed(e) {
     boxobj.matrixAutoUpdate=false;
-    //boxobj.matrixWorldNeedsUpdate = true;
+    boxobj.matrixWorldNeedsUpdate = true;
     let objectmat=new THREE.Matrix4();
     let mat_r=new THREE.Matrix4();
     let mat_t=new THREE.Matrix4();
     objectmat=boxobj.matrix;
-    //objectmat = mat_t.premultiply(mat_r);
 
 	switch(e.keyCode) {
         // 1) rotation
@@ -119,35 +99,24 @@ function checkKeyPressed(e) {
         // 2) translation
         case 97: // 'a'
         mat_t= new THREE.Matrix4().makeTranslation(-trans(-10,0),0,0);
-        //boxobj.matrix.multiply(mat_t);
         boxobj.matrix.premultiply(mat_t);
             break;	     
         case 100: // 'd'
         mat_t= new THREE.Matrix4().makeTranslation(trans(10,0),0,0);
-        //boxobj.matrix.multiply(mat_t);
         boxobj.matrix.premultiply(mat_t);
 
             break;	 
         case 119: // 'w'
         mat_t= new THREE.Matrix4().makeTranslation(0,trans(0,10),0);
-       // boxobj.matrix.multiply(mat_t);
        boxobj.matrix.premultiply(mat_t);
             break;	 
         case 115: // 's'
         mat_t= new THREE.Matrix4().makeTranslation(0,-trans(0,-10),0);
-       // boxobj.matrix.multiply(mat_t);
        boxobj.matrix.premultiply(mat_t);
             break;	 
 	}	
-    
 }
 
 renderer.setAnimationLoop(() => {
     renderer.render(scene, camera);
 });
-
-
-
-
-
-
