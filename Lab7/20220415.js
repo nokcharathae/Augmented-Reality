ef6af8c7ef9baf29ae8e92e97bc1de07525d5284
nokcharathae <rkwmal0000@naver.com>
2022-04-18 11:44:46 +0900

    upload

--- Lab7/20220415.js
+++ Lab7/20220415.js
@@ -14,7 +14,8 @@ renderer.setViewport(0,0,render_w,render_h);
 document.body.appendChild(renderer.domElement);
 
 const camera_ar=new THREE.PerspectiveCamera(75, render_w,render_h,1,500);
-camera_ar.position.set(0,0,10);
+// near plane은 (0,0,99)에 위치
+camera_ar.position.set(0,0,100);
 camera_ar.lookAt(0,0,0);
 camera_ar.up.set(0,1,0);
 
@@ -23,6 +24,8 @@ const scene=new THREE.Scene();
 const texture_bg=new THREE.VideoTexture(videoElement);
 scene.background = texture_bg;
 
+let point_mesh;
+
 function onResults(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
@@ -30,7 +33,10 @@ function onResults(results) {
       results.image, 0, 0, canvasElement.width, canvasElement.height);
   if (results.multiFaceLandmarks) {
     for (const landmarks of results.multiFaceLandmarks) {
-      console.log(landmarks[300]);
+      // console.log(landmarks[300]);
+      // canvas에 lendmarks를 찍음
+      // console.log(FACEMESH_RIGHT_IRIS); // landmark 점의 index
+      // TESSELATION : interpolation -> 내부 mesh를 그림
       drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
                      {color: '#C0C0C070', lineWidth: 1});
       drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
@@ -41,6 +47,28 @@ function onResults(results) {
       drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
       drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
       drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+
+      // FACEMESH_FACE_OVAL, landmarks
+      if(point_mesh==null){
+        let oval_point_geo=new THREE.BufferGeometry();
+        const num_oval_points=FACEMESH_FACE_OVAL.length;
+        const oval_vertices =[];
+        // const oval_vertices = new Float32Array( num_oval_points );
+        for(let i=0;i<num_oval_points;i++){
+          const index=FACEMESH_FACE_OVAL[i][0];
+          const pos_ns=landmarks[index];
+          const pos_ps=new THREE.Vector3((pos_ns.x-0.5)*2,-(pos_ns.y-0.5)*2,pos_ns.z);
+          let pos_ws=new THREE.Vector3(pos_ps.x,pos_ps.y,pos_ps.z).unproject(camera_ar);
+          // oval_vertices[i]=pos_ws;
+          oval_vertices.push(pos_ws.x,pos_ws.y,pos_ws.z);
+        }
+        const poin_mat=new THREE.PointsMaterial({color:0xFF0000,size:0.02});
+        //oval_point_geo.setFromPoints(oval_vertices);
+        oval_point_geo.setAttribute('position',new THREE.Float32BufferAttribute(oval_vertices,3));
+        point_mesh=new THREE.Points(oval_point_geo, poin_mat);
+        scene.add(point_mesh);
+      }
+      point_mesh.geometry;
     }
   }
   renderer.render(scene, camera_ar);
